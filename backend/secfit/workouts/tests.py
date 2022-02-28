7650bf74f201964a2a35829c7f2af8098eb47a0a
kjerand <kjerand.evje@gmail.com>
2022-02-28 13:58:58 +0100

    tests

--- backend/secfit/workouts/tests.py
+++ backend/secfit/workouts/tests.py
@@ -1,6 +1,55 @@
 """
 Tests for the workouts application.
 """
-from django.test import TestCase
+from urllib import request
+from django.test import TestCase, RequestFactory
+from workouts.permissions import (
+    IsOwner,
+    IsCoachAndVisibleToCoach,
+    IsOwnerOfWorkout,
+    IsCoachOfWorkoutAndVisibleToCoach,
+    IsReadOnly,
+    IsPublic,
+    IsWorkoutPublic,
+)
+from workouts.serializers import (
+    WorkoutFileSerializer
+)
+
+
+class WorkoutPermissionsTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        
+    def test_is_owner(self):
+        is_owner_object = IsOwner()
+        workout_test_object = WorkoutFileSerializer()
+
+        request = self.factory.get("/")
+
+        workout_test_object.owner = "Ola Nordmann"
+
+        request.user = workout_test_object.owner
+
+        self.assertEqual(IsOwner.has_object_permission(is_owner_object, request, "view", workout_test_object), True)
+
+    def test_is_workout_owner(self): 
+        self.assertEqual(5, 5)
+
+    def test_is_coach_and_visible(self):
+        is_coach_object = IsCoachAndVisibleToCoach()
+        workout_test_object = WorkoutFileSerializer()
+
+        request = self.factory.get("/")
+
+        workout_test_object.owner.coach = "Ola Nordmann"
+
+        request.user = workout_test_object.owner.coach
+       
+        self.assertEqual(IsCoachAndVisibleToCoach.has_object_permission(is_coach_object, request, "view", workout_test_object), True)
+
+
+
+
 
 # Create your tests here.
