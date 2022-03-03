327ac94f2fdcf21f2c40e3e6adbc036855b2a9d6
kjerand <kjerand.evje@gmail.com>
2022-03-03 10:43:46 +0100

    test fail

--- backend/secfit/users/tests.py
+++ backend/secfit/users/tests.py
@@ -16,6 +16,13 @@ class UserSerializerTest(TestCase):
 
         self.assertEqual(UserSerializer.validate_password(user_serializer_object, password), password)
 
+        password = " "
+        user_serializer_object.password = password
+        user_serializer_object.password1 = password
+
+        with self.assertRaises(ValidationError):
+            UserSerializer.validate_password(user_serializer_object, password)
+
 
     def test_create_user(self):
         user_serializer_object = UserSerializer()
